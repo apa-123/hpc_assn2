d6b25d086797e4ef77a1d03a4b054df312edf822
Aparajitha Adiraju <aparajitha@gmail.com>
2020-04-15 16:15:51 -0700

    fixed num one

--- one.c
+++ one.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 void main() {
-    #pragma omp parallel {
+    #pragma omp parallel
+    {
     int ID = omp_get_thread_num();
     printf(" hello(%d) ", ID);
     printf(" world(%d) \n", ID);
