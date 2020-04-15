ff6ad6d31e8adf655f5ef585bff0fbaa80f29c19
Aparajitha <adiraju2@illinois.edu>
2020-04-15 14:47:51 -0500

    first commit

---
+++ one.c
@@ -0,0 +1,10 @@
+#include "omp.h"
+#include <stdio.h>
+
+void main() {
+    #pragma omp parallel {
+    int ID = omp_get_thread_num();
+    printf(" hello(%d) ", ID);
+    printf(" world(%d) \n", ID);
+    }
+}
