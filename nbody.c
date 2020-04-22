bc022c11e196b1e5a90dff78280b96aa4870184e
Aparajitha <adiraju2@illinois.edu>
2020-04-21 23:48:13 -0500

    reg code

--- nbody.c
+++ nbody.c
@@ -9,66 +9,81 @@
 #define DIM 2
 typedef double vect_t[DIM];
 
-int main(int argc, char *argv[]) {
-	n = argv[1];
-	delta_t = argv[2]
-	T = argv[3]
+int main() {
+	int n = 500;
+	float delta_t = 0.05;
+	int T = 100;
+	float x_diff;
+	float y_diff;
+	float dist;
+	float dist_cubed; 
+	float forcex;
+	float forcey;
 
-	vect_t∗ forces = malloc(n∗sizeof(vect_t));
+	vect_t* forces = malloc(n * sizeof(vect_t));
 	vect_t* vel = malloc(n*sizeof(vect_t));
 	vect_t* pos = malloc(n*sizeof(vect_t));
-	vect_t* mass = malloc(n*sizeof(vect_t));
-	
-	for (int step = 1; step <= n; steps++) { 
-		forces = memset(forces, 0, n*sizeof(vect_t));
-		vel = memset(vel, 0, n*sizeof(vect_t));
-		pos = memset(pos, 0, n*sizeof(vect_t));
-		mass = memset(mass, 0, n*sizeof(vect_t));
+	vect_t* old_pos = malloc(n*sizeof(vect_t));
+	double* mass = malloc(n*sizeof(int));
 
+	for (int step = 0; step <= T; step++) { 
+		memset(forces, 0, n*sizeof(vect_t));
+		memset(vel, 0, n*sizeof(vect_t));
+		memset(pos, 0, n*sizeof(vect_t));
+		memset(old_pos, 0, n*sizeof(vect_t));
 		// initialization
-		for (int q = 0; q < n-1; q++) {
-        		forces[q] = 0;
+		for (int q = 0; q < n; q++) {
+        	forces[step][q] = 0;
 			pos[q][X] = (rand() / (double)(RAND_MAX)) * 2 - 1;
 			pos[q][Y] = (rand() / (double)(RAND_MAX)) * 2 - 1;
-			pos[q][Z] = (rand() / (double)(RAND_MAX)) * 2 - 1;
 
-			old_pos[i][X] = pos[q][X];
-			old_pos[i][Y] = pos[q][Y];
-			old_pos[i][Z] = pos[q][Z];
+			old_pos[q][X] = pos[q][X];
+			old_pos[q][Y] = pos[q][Y];
 
 			vel[q][X] = (rand() / (double)(RAND_MAX)) * 2 - 1;
 			vel[q][Y] = (rand() / (double)(RAND_MAX)) * 2 - 1;
-			vel[q][Z] = (rand() / (double)(RAND_MAX)) * 2 - 1;
 
 			mass[q] = fabs((rand() / (double)(RAND_MAX)) * 2 - 1);
 		}
-		// not sure if it's till n-1 or n
-		for (int q = 0; q < n-1; q++) {
+// Reduced algorithm
+		for (int q = 0; q < n; q++) {
 			// compute forces
-			for (int k = q+1; k < n-1; k++) {
-			    x_diff = pos[q][X] − pos[k][X];
-			    y_diff = pos[q][Y] − pos[k][Y];
-			    dist = sqrt(x_diff∗x diff + y_diff∗y diff);
-			    dist_cubed = dist∗dist∗dist;
-			    force_qk[X] = G∗masses[q]∗masses[k]/dist_cubed ∗ x_diff;
-			    force_qk[Y] = G∗masses[q]∗masses[k]/dist_cubed ∗ y_diff
-			    forces[q][X] += force_qk[X];
-			    forces[q][Y] += force_qk[Y];
-			    forces[k][X] −= force qk[X];
-			    forces[k][Y] −= force qk[Y];
+			for (int k = q+1; k < n; k++) {
+			    x_diff = pos[q][X] - pos[k][X];
+			    y_diff = pos[q][Y] - pos[k][Y];
+			    dist = sqrt(x_diff * x_diff + y_diff * y_diff);
+			    dist_cubed = dist * dist * dist;
+			    forcex = G * mass[q] * mass[k]/dist_cubed * x_diff;
+			    forcey = G * mass[q] * mass[k]/dist_cubed * y_diff;
+			    forces[q][X] += forcex;
+			    forces[q][Y] += forcey;
+			    forces[k][X] -= forcex;
+			    forces[k][Y] -= forcey;
 			}
 		}
+// Simple algorithm 
+		// for (int q = 0; q < n; q++) {
+		// 	for (int k = 0; k < n && k != q; k++) {
+		// 	    x_diff = pos[q][X] - pos[k][X];
+		// 	    y_diff = pos[q][Y] - pos[k][Y];
+		// 	    dist = sqrt(x_diff * x_diff + y_diff * y_diff);
+		// 	    dist_cubed = dist * dist * dist;
+		// 	    forces[q][X] = G * mass[q] * mass[k]/dist_cubed * x_diff;
+		// 	    forces[q][Y] = G * mass[q] * mass[k]/dist_cubed * y_diff;
+		// 	}
+		// }
+		
 		for (int q = 0; q < n-1; q++) { 
-			pos[q][X] += delta_t∗vel[q][X];
-			pos[q][Y] += delta_t∗vel[q][Y];
-			vel[q][X] += delta_t/masses[q]∗forces[q][X];
-			vel[q][Y] += delta_t/masses[q]∗forces[q][Y];
-
+			pos[q][X] += delta_t * vel[q][X];
+			pos[q][Y] += delta_t * vel[q][Y];
+			vel[q][X] += delta_t/mass[q] * forces[q][X];
+			vel[q][Y] += delta_t/mass[q] * forces[q][Y];
+		}
 	}
 	
 	for (int i = 0; i < n; i++) {
-		std::cout << "position: " << pos[X] << " " << pos[Y] << std::endl;
-		std::cout << "velocity: " << vel[X] << " " << vel[Y] << std::endl;
+		printf("position: %f %f ", pos[i][X], pos[i][Y]);
+		printf("velocity: %f %f\n",vel[i][X], vel[i][Y]);
 	}
 	return 0;
 } 
