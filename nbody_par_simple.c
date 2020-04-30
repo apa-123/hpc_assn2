7a11263640e74bca1dd9424dc99781f95a6fdadc
Aparajitha <adiraju2@illinois.edu>
2020-04-29 23:12:41 -0500

    test

--- nbody_par_simple.c
+++ nbody_par_simple.c
@@ -33,7 +33,7 @@ int main() {
 		memset(pos, 0, n*sizeof(vect_t));
 		memset(old_pos, 0, n*sizeof(vect_t));
 		// initialization
-        #pragma omp parallel for schedule(static, 5)
+        // #pragma omp parallel for schedule(static, 5)
    // 	#pragma omp parallel for schedule (guided, 5)
 			for (int q = 0; q < n; q++) {
 				forces[step][q] = 0;
